7be2f76120a69d74318241497b6a704dd413a5aa
sundownspecial <je.mendoza@live.com>
2018-03-20 20:42:46 -0700

    not populating db on the blockschema schema

--- models/block.js
+++ models/block.js
@@ -2,12 +2,13 @@
 
 const mongoose = require('mongoose');
 
-const Block = mongoose.Schema({
-  block_number: {type: Number, required: true},
-  previous_hash: {type: String, required: true},
-  hash: {type: String, required: true},
+const BlockSchema = mongoose.Schema({
+  blocknumber: {type: Number, required: true},
+  previoushash: {type: String, required: true},
   transactions: [],
+  timestamp: {type: Number, required: true},
+  hash: {type: String, required: true},
   nonce: {type: Number, required: true},
-}, {timestamps: true});
+});
 
-module.exports = mongoose.model('block', Block);
+module.exports = mongoose.model('blockSchema', BlockSchema);
